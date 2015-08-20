72913cc6fffc699cb62b7e7fd4934fb331cf48e1
dyanac <daniel.yanac@ucsp.edu.pe>
2015-08-20 16:40:23 -0500

    cambios

--- invertir.py
+++ invertir.py
@@ -2,20 +2,11 @@
 a = raw_input("ingresar numero: ")
 b = " "
 c = len(a)-1
+lista=[]
 for i in range(c+1):
 		b+=a[c]
 		c-=1
 		print+str(b)
-<<<<<<< HEAD
 
-#asdaweas
-=======
-		
-#asdadasd
-
-for i in range (10):
-	print "asda"
->>>>>>> 62feb5cb809f050169914fa4872bfdf69841f045
-
-for j in range (10):
-	print "nada"
+for i in range (8):
+	lista+=[i]
